8ac0ab66239fedadf2cc02849ed752fb249b1479
juliavila <julcar.as@gmail.com>
2024-05-10 12:45:50 -0500

    Se muestran 3 mensajes

---
+++ EjemploGit/src/ejemplogit/EjemploGit.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejemplogit;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EjemploGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola GitHub");
+        System.out.println("Otra cosa");
+        System.out.println("Algo mas");
+    }
+    
+}
